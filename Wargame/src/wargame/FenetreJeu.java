1a206f4f8c78cb4265a94373e545f66821c2fce9
f_marion <florianmarion38300@gmail.com>
2025-11-28 18:32:09 +0100

    ajout d'un main dans FenetreJeu et affichage d'une carte tres basique

--- Wargame/src/wargame/FenetreJeu.java
+++ Wargame/src/wargame/FenetreJeu.java
@@ -1,5 +1,26 @@
 package wargame;
 
-public class FenetreJeu {
+import java.awt.*; 
+import javax.swing.*;
 
+public class FenetreJeu {
+	public static void main(String[] args) {
+		Obstacle o = new Obstacle(Obstacle.TypeObstacle.ROCHER, new Position(3, 5));
+		Carte c = new Carte();
+		c.setElement(o, o.getPos());
+		// Creer la JFrame
+		JFrame frame = new JFrame("Fenêtre Jeu");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Créer la JMenuBar
+		
+		frame.setLocation(100, 100);
+		// Afficher...
+		JPanel panel = new PanneauJeu(c);
+		panel.setPreferredSize(new Dimension(1920,1080));
+		panel.setLocation(100, 100);
+		panel.setBackground(Color.GRAY);
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		frame.pack();
+		frame.setVisible(true); // Car masqué par défaut
+	}
 }
--- Wargame/src/wargame/PanneauJeu.java
+++ Wargame/src/wargame/PanneauJeu.java
@@ -1,5 +1,39 @@
 package wargame;
 
-public class PanneauJeu {
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
 
+public class PanneauJeu extends JPanel {
+	private static final long serialVersionUID = 1L;
+	JButton bouton;
+	JLabel label;
+	JPanel carre[][] = new JPanel[IConfig.HAUTEUR_CARTE][IConfig.LARGEUR_CARTE];
+	
+	int cmp = 0;
+	public PanneauJeu(Carte c) {
+		int x,y,i,j;
+		Position p = new Position();
+		Element e = new Element();
+		setLayout(new GridLayout(IConfig.HAUTEUR_CARTE, IConfig.LARGEUR_CARTE));
+		for(i = 0 ; i < IConfig.HAUTEUR_CARTE ; i++) {
+			for(j = 0 ; j < IConfig.LARGEUR_CARTE ; j++) {
+				x = i;
+				y = j;
+				
+				carre[x][y] = new JPanel();
+				p.setX(x);
+				p.setY(y);
+				e = c.getElement(p);
+				
+				
+				carre[x][y].setBorder(BorderFactory.createLineBorder(Color.BLACK));
+				
+				if(e instanceof Obstacle) {
+					carre[x][y].setBackground(Color.red);
+				}
+				add(carre[x][y]);
+			}
+		}
+	}
 }
